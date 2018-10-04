4bdf3c7547bfc2b5bbeee5c8da4d41ac93760193
Camila Bueno <cbf.camila@gmail.com>
2018-10-04 15:52:50 -0300

    Create function to Open Weather

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=campinas,br&APPID=fe9fa986796faa87ef8b7a803ed0197d&units=metric&lang=pt")
     .then(function(response) { return response.json(); })
-    .then(function(data) { createAll(weathers.list) });
+    .then(function(data) {createAll(data.list) });
+  
 });
-$(document).ready(function() {
-    var weatherToday = [];
-    for (i = 0;i < weather.length;i++) {
-      weatherList = weather[i]['list'];
-      weatherToday.push(weatherList);
-    }
-})
+function createAll(weatherListData){
+    
+   let diaAtual = weatherListData[0].main.temp;
+   let tempMin = weatherListData[0].main.temp_min;
+   let tempMax = weatherListData[0].main.temp_max;
+   let description = weatherListData[0].weather[0].description;
+   
+   
+   $('#weather').append(`
+   
+   <h1>Temperatura Agora: ${diaAtual}</h1>
+   <h3>Temperatura mínima: ${tempMin}</h3><br>
+   <h3>Temperatura máxima: ${tempMax}</h3>
+   <h2> Descrição: ${description}</h2>`);     
+    
+
+
+}
