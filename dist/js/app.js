2edf4f34a0d4df0a44c04b53b6e5c0b33d6c8567
Camila Bueno <cbf.camila@gmail.com>
2018-10-04 13:28:36 -0300

    Add app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     fetch("https://api.openweathermap.org/data/2.5/forecast?q=campinas,br&APPID=fe9fa986796faa87ef8b7a803ed0197d&units=metric&lang=pt")
     .then(function(response) { return response.json(); })
-    .then(function(data) { createAll(data.list) });
-  });
+    .then(function(data) { createAll(weathers.list) });
+
 
 
   function createAll(weatherList) {
       $('#weather').html('');
 
          $.each(weatherArray, function( weathers){
-             $('')
+             $('<img>')
              .attr('', weathers.list)
              .attr('', weathers[0])
              .addClass(weatherToday)
@@ -19,3 +19,5 @@ $(document).ready(function(){
   }
 
     return createAll(weatherList);
+
+});
